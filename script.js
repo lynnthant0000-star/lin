af275d34c75b6e31e9ffe7405b4742c097163bca
lynnthant0000-star <lynnthant0000@gmail.com>
2026-04-16 06:17:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function sayHello() {
-  alert("Hello! mwahh love u very much 💐💐💐");
-function showPhoto() {
-  document.getElementById("photo").style.display = "block";
-}
+function showContent() {
+  let photo = document.getElementById("photo");
+  let text = document.getElementById("text");
 
+  if (photo.style.display === "none") {
+    photo.style.display = "block";
+    text.style.display = "block";
+  } else {
+    photo.style.display = "none";
+    text.style.display = "none";
+  }
+}
